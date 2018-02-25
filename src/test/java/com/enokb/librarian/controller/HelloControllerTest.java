2f778723759c9dc579e8543fbe7fab78f763eb90
ksewen <261517946@qq.com>
2018-02-25 15:17:12 +0800

    add unit test for HelloController

---
+++ src/test/java/com/enokb/librarian/controller/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.enokb.librarian.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * @program: librarian
+ * @description:
+ * @author: ksewen
+ * @create: 2018-02-25 14:42
+ **/
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void before() { }
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
