5e76ad69c8fb49564b364e5b6b7ccabde66cad4b
ksewen <261517946@qq.com>
2018-03-22 17:07:25 +0800

    add logback.xml and config mybatis level DEBUG fix booksearch api

--- src/main/java/com/enokb/librarian/mapper/BookExtMapper.java
+++ src/main/java/com/enokb/librarian/mapper/BookExtMapper.java
@@ -2,6 +2,7 @@ package com.enokb.librarian.mapper;
 
 import com.enokb.librarian.domain.BookDomain;
 import com.enokb.librarian.model.BookSearchModel;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -18,10 +19,10 @@ public interface BookExtMapper {
     @Select("<script>SELECT isbn, name, price, type, author, press " +
             "FROM book " +
             "WHERE 1=1 " +
-            "<if test='#{condition.isbn} != null'>AND isbn=#{condition.isbn}</if> " +
-            "<if test='#{condition.name} != null'>AND LOCATE(#{condition.name}, `name`)>0</if> " +
-            "<if test='#{condition.type} != null'>AND type=#{condition.type}</if> " +
-            "<if test='#{condition.author} != null'>AND LOCATE(#{condition.author}, `author`)>0</if> " +
-            "<if test='#{condition.press} != null'>AND LOCATE(#{condition.press}, `press`)>0</if> </script>")
-    List<BookDomain> searchBook(BookSearchModel bookSearchModel);
+            "<if test='condition.isbn != null and condition.isbn != \"\"'>AND isbn=#{condition.isbn}</if> " +
+            "<if test='condition.name != null and condition.name != \"\"'>AND LOCATE(#{condition.name}, `name`)>0</if> " +
+            "<if test='condition.type != null and condition.type != \"\"'>AND type=#{condition.type}</if> " +
+            "<if test='condition.author != null and condition.author != \"\"'>AND LOCATE(#{condition.author}, `author`)>0</if> " +
+            "<if test='condition.press != null and condition.press != \"\"'>AND LOCATE(#{condition.press}, `press`)>0</if> </script>")
+    List<BookDomain> searchBook(@Param("condition") BookSearchModel bookSearchModel);
 }
